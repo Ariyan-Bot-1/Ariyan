986c801a6bd548c682a6cd377c29e37192abc6b8
Ariyan-Bot-1 <ariyanahmed6393@gmail.com>
2025-10-25 01:29:48 +0600

    Update Bot.js

--- Script/commands/Bot.js
+++ Script/commands/Bot.js
@@ -91,7 +91,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("সরি বস মাফ করে দেন আর এমন ভুল হবে না🥺🙏", threadID);
    };
 
-   if ((event.body.toLowerCase() == "@Farhana Ontora") || (event.body.toLowerCase() == "@Farhana Ontora ")) {
+   if ((event.body.toLowerCase() == "@一aɽïyaɳ ꪹ٭ぎ.") || (event.body.toLowerCase() == "@一aɽïyaɳ ꪹ٭ぎ. ")) {
      return api.sendMessage("খবরদার কেউ এই আইড়ি মেনশন দিবানা এটা আমার বস উল্লাস এর বউ এর আইড়ি😠🥰⛏️", threadID);
    };
 
